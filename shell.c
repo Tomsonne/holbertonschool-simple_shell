0af346d7ec156bb02417e5259fc88f86fe397c10
thomas rousseau <thomas.rousseau19@gmail.com>
2025-04-21 20:06:01 +0200

    ajout fonctionnalite env/setenv/unsetenv + prototype

--- shell.c
+++ shell.c
@@ -17,31 +17,33 @@ int simple_shell(char *program_name)
 	{
 		if (interactive)
 			shell_prompt();
-
 		if (getline(&line, &len, stdin) == -1)
 		{
 			if (interactive)
 				write(1, "\n", 1);
 			break;
 		}
-
 		args = split_line(line);
 		if (!args || !args[0])
 		{
 			free(args);
 			continue;
 		}
-
 		if (strcmp(args[0], "exit") == 0)
 		{
 			free(args);
 			break;
 		}
-
+		else if (strcmp(args[0], "env") == 0)
+			executer_env();
+		else if (strcmp(args[0], "setenv") == 0)
+			executer_setenv(args);
+		else if (strcmp(args[0], "unsetenv") == 0)
+			executer_unsetenv(args);
+		else
 		execute_command(args, program_name);
 		free(args);
 	}
-
 	free(line);
 	return (0);
 }
